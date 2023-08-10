b819b392ba2ad345acb678034d67d087a359001f
B Kanhu Charan <iamkanhuc@gmail.com>
2023-08-10 18:13:21 +0530

    update sidebat

--- components/SidebarOne.js
+++ components/SidebarOne.js
@@ -80,6 +80,7 @@ const SidebarOne = () => {
             src="/blue_short-8e7b30f73a4020692ccca9c88bafe5dcb6f8a62a4c6bc55cd9ba82bb2cd95f6c.svg"
             height={80}
             width={100}
+            alt="imahe"
           />
         </a>
       </p>
